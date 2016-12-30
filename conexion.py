29d79bd2252de9145975005e3708cb8275f758c7
cesarmarquezmedina <cesar.marquez.medina@outlook.com>
2016-12-30 10:23:37 -0600

    Delete conexion.py

--- conexion.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-cnx = mysql.connector.connect(user='cesar', password='CESAR_2606952_*cmarquez',
-                              host='127.0.0.1',
-                              database='Urban',
-                              port='3306')
-cnx.ping(attempts=3, delay=0)
-cnx.close()
