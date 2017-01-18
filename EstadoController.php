3130c1ad26c13c9e1b87b04e98b2f6abe23010c7
cesarmarquezmedina <cesar.marquez.medina@outlook.com>
2017-01-18 12:17:31 -0600

    Add files via upload

---
+++ EstadoController.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+
+
+class EstadoController extends Controller
+
+{
+    
+	public function show($lat, $long)
+	{
+                               
+		$data = array($lat, $long);
+               
+		$result = shell_exec('python /var/www/html/Urban/app/Http/Controllers/Scripts/Estado.py ' . escapeshellarg(json_encode($data)));
+
+     		$resultData = json_decode($result, true);
+    
+           	return($resultData);
+        
+	}
+}
